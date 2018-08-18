cf2bf77c3b7c97f303a84605ee0f578e35a3f6ab
Kuthumi Pepple <themasterkhp@gmail.com>
2018-08-18 20:27:54 +0100

    Add methods for app behaviour to Main Activity

--- app/src/main/java/com/example/android/basketballcounter/MainActivity.java
+++ app/src/main/java/com/example/android/basketballcounter/MainActivity.java
@@ -2,12 +2,92 @@ package com.example.android.basketballcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+    private TextView scoreView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreView = (TextView) findViewById(R.id.team_b_score);
+    }
+
+    /**
+     * Adds 3 points to the score of Team A
+     */
+    public void addThreeForTeamA(View v){
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Adds 2 points to the score of Team A
+     */
+    public void addTwoForTeamA(View v){
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Adds 3 points to the score of Team A
+     */
+    public void addOneForTeamA(View v){
+        ++scoreTeamA;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A
+     */
+    public  void displayForTeamA(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Adds 3 points to the score of Team B
+     */
+    public void addThreeForTeamB(View v){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Adds 2 points to the score of Team B
+     */
+    public void addTwoForTeamB(View v){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Adds 3 points to the score of Team B
+     */
+    public void addOneForTeamB(View v){
+        ++scoreTeamB;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team B
+     */
+    public  void displayForTeamB(int score){
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Resets the scores of Team A and Team B to 0
+     */
+    public void resetScores(View v){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
+
